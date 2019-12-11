5c7b5fc653eb8406433484facbaf3f4b9a3312be
annapry <annaprilipko13@gmail.com>
2019-12-11 17:51:56 +0200

    updated

--- react-firebase-authentication/src/components/Landing/out.js
+++ react-firebase-authentication/src/components/Landing/out.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LandingOut = () => (
   <div>
     <h1>Welcome to realtime chatroom</h1>
-    <h2>Go to Chat room to start messaging</h2>
+    <h2>Go to Chat room to start messaging or sign up for that</h2>
   </div>
 );
 
